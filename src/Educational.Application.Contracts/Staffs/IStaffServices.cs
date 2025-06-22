10c2745adcda5d0cf9363006d92007f7cd49ab3d
wxmhuhu <3513817181@qq.com>
2025-06-22 16:20:55 +0800

    登录，新增
    登录，新增

---
+++ src/Educational.Application.Contracts/Staffs/IStaffServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Educational.Staffs;
+
+
+namespace Educational.Staffs
+{
+    public interface IStaffServices:IApplicationService
+    {
+        Task<ApiResult<StaffInfo>> AddStaff(StaffInfo staffInfo);
+        Task<ApiResult<StaffInfo>> Login(LoginDTO loginDTO);
+    }
+}
---
+++ src/Educational.Application.Contracts/Staffs/LoginDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educational.Staffs
+{
+    public class LoginDTO
+    {
+        /// <summary>
+        /// 电话/登录账号（必填）
+        /// </summary>
+        public string StaffAccount { get; set; }
+        /// <summary>
+        /// 电话/登录账号（必填）
+        /// </summary>
+        public string StaffPassword { get; set; }
+    }
+}
