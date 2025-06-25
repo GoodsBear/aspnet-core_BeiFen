5c8defea70c909bc6762bd5ef602638c56b151d1
小困鱼0329 <2998198261@qq.com>
2025-06-25 20:56:26 +0800

    角色分配权限

--- src/Educational.HttpApi.Host/EducationalHttpApiHostModule.cs
+++ src/Educational.HttpApi.Host/EducationalHttpApiHostModule.cs
@@ -56,7 +56,7 @@ public class EducationalHttpApiHostModule : AbpModule
 
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        // ��ȡ���ö���
+        // 配置认证
         var configuration = context.Services.GetConfiguration();
         var hostingEnvironment = context.Services.GetHostingEnvironment();
 
@@ -173,7 +173,8 @@ public class EducationalHttpApiHostModule : AbpModule
                 options.SwaggerDoc("权限", new OpenApiInfo { Title = "权限管理", Version = "v1" });
                 options.SwaggerDoc("角色", new OpenApiInfo { Title = "角色管理", Version = "v1" });
                 options.SwaggerDoc("成员", new OpenApiInfo { Title = "成员管理", Version = "v1" });
-
+                options.SwaggerDoc("成员分配角色", new OpenApiInfo { Title = "成员分配角色管理", Version = "v1" });
+                options.SwaggerDoc("角色分配权限", new OpenApiInfo { Title = "角色分配权限管理", Version = "v1" });
                 options.DocInclusionPredicate((doc, desc) =>
                 {
                     if (!desc.GroupName.IsNullOrWhiteSpace())
@@ -282,7 +283,8 @@ public class EducationalHttpApiHostModule : AbpModule
             c.SwaggerEndpoint("/swagger/权限/swagger.json", "权限管理 v1");
             c.SwaggerEndpoint("/swagger/角色/swagger.json", "角色管理 v1");
             c.SwaggerEndpoint("/swagger/成员/swagger.json", "成员管理 v1");
-
+            c.SwaggerEndpoint("/swagger/成员分配角色/swagger.json", "成员分配角色管理 v1"); 
+            c.SwaggerEndpoint("/swagger/角色分配权限/swagger.json", "角色分配权限管理 v1");
             var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
             c.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
             c.OAuthScopes("Educational");
