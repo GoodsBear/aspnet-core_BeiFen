0bcbb6fec7a85ddcaa31492da4284d2056641383
小困鱼0329 <2998198261@qq.com>
2025-06-26 15:35:22 +0800

    添加decimal限制

--- src/Educational.Domain/Classgrade/ClassRoom.cs
+++ src/Educational.Domain/Classgrade/ClassRoom.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,7 @@ namespace Educational.Classgrade
         /// </summary>
         [Required(ErrorMessage = "面积不能为空")]
         [Range(0, 10000, ErrorMessage = "面积不能小于0或大于10000")]
+        [Column(TypeName = "decimal(8,3)")]
         public decimal ClassRoomArea { get; set; }
         /// <summary>
         /// 备注
--- src/Educational.EntityFrameworkCore/Migrations/20250626071510_小数点限制.Designer.cs
+++ src/Educational.EntityFrameworkCore/Migrations/20250626071510_小数点限制.Designer.cs
@@ -4,6 +4,7 @@ using Educational.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Educational.Migrations
 {
     [DbContext(typeof(EducationalDbContext))]
-    partial class EducationalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250626071510_小数点限制")]
+    partial class 小数点限制
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
