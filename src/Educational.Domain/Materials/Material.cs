b71684a7b74389f1457a73dc91fb680160a17fc5
liyatao751992452 <751992452@qq.com>
2025-06-26 10:28:26 +0800

    添加验证特性

--- src/Educational.Domain/Materials/Material.cs
+++ src/Educational.Domain/Materials/Material.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Educational.Materials
@@ -12,17 +13,44 @@ namespace Educational.Materials
 	/// </summary>
 	public class Material: FullAuditedAggregateRoot<Guid>
 	{
-		//物料名称
-        public string MaterialName { get; set; }
-		//图片
-        public string MaterialImage { get; set; }
-		//物料分类
-        public MaterialTypeEnum MaterialTypeId { get; set; }
-		//所属学校
-        public Guid SchoolId { get; set; }
-		//库存
-        public int StockSum { get; set; }
-		//状态
+		/// <summary>
+		/// 物料名称
+		/// </summary>
+		[Required(ErrorMessage = "物料名称不能为空")]
+		[StringLength(100, MinimumLength = 2, ErrorMessage = "物料名称长度需在2-100个字符之间")]
+		public string MaterialName { get; set; }
+
+		/// <summary>
+		/// 图片
+		/// </summary>
+		[Url(ErrorMessage = "图片地址格式不正确")]
+		[MaxLength(500, ErrorMessage = "图片地址长度不能超过500字符")]
+		public string MaterialImage { get; set; }
+
+		/// <summary>
+		/// 物料分类
+		/// </summary>
+		[EnumDataType(typeof(MaterialTypeEnum), ErrorMessage = "请选择有效的物料分类")]
+		public MaterialTypeEnum MaterialTypeId { get; set; }
+
+		/// <summary>
+		/// 所属学校
+		/// </summary>
+		[Required(ErrorMessage = "必须指定所属学校")]
+		[RegularExpression(@"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
+			ErrorMessage = "学校ID必须是有效的GUID格式")]
+		public Guid SchoolId { get; set; }
+
+		/// <summary>
+		/// 库存
+		/// </summary>
+		[Range(0, 100000, ErrorMessage = "库存数量必须在0-100000之间")]
+		public int StockSum { get; set; }
+
+		/// <summary>
+		/// 状态
+		/// </summary>
+		[Required(ErrorMessage = "必须指定物料状态")]
 		public bool Status { get; set; }
 	}
 }
