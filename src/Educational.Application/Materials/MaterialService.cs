957ded36583ffa8afac2c2caaaad5686ee404b7b
liyatao751992452 <751992452@qq.com>
2025-06-27 19:08:36 +0800

    嗯哼

--- src/Educational.Application/Materials/MaterialService.cs
+++ src/Educational.Application/Materials/MaterialService.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Linq;
+using Volo.Abp.Uow;
 
 namespace Educational.Materials
 {
@@ -91,6 +92,7 @@ namespace Educational.Materials
 		/// <returns></returns>
 		/// <exception cref="NotImplementedException"></exception>
 		[HttpPut]
+		[UnitOfWork]
 		public async Task<ApiResult> UpdataStatus(List<Guid> ids,bool status)
 		{
 			try
